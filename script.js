052d2fcaf7cd7783679887d059a1faf5e096abe8
White Tiger <dayley.zimmermann@gmail.com>
2022-10-18 20:33:22 -0400

    added debug mode

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ window.addEventListener("load", function () {
           this.game.keys.push(e.key);
         } else if (e.key === " ") {
           this.game.player.shootTop();
+        } else if (e.key === "d") {
+          this.game.debug = !this.game.debug;
         }
-        console.log(this.game.keys);
       });
       window.addEventListener("keyup", (e) => {
         if (this.game.keys.indexOf(e.key) > -1) {
           this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
         }
-        console.log(this.game.keys);
       });
     }
   }
@@ -85,8 +85,8 @@ window.addEventListener("load", function () {
       }
     }
     draw(context) {
-      context.fillStyle = "black";
-      context.fillRect(this.x, this.y, this.width, this.height);
+      if (this.game.debug)
+        context.strokeRect(this.x, this.y, this.width, this.height);
       context.drawImage(
         this.image,
         this.frameX * this.width,
@@ -259,6 +259,7 @@ window.addEventListener("load", function () {
       this.gameTime = 0;
       this.timeLimit = 5000;
       this.speed = 1;
+      this.debug = true;
     }
     update(deltaTime) {
       if (!this.gameOver) this.gameTime += deltaTime;
